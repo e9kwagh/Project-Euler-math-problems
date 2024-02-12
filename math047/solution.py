ac4c84e8b9a111789a7933ccf7dc5808a14d4ca3
Kunal <waghkunal1998@gmail.com>
2024-02-12 16:07:37 +0530

    new commit

--- math047/solution.py
+++ math047/solution.py
@@ -1,7 +1,8 @@
-"""math047"""
+"""math47"""
 
 
 def is_prime(num):
+    """prime"""
     if num < 2:
         return False
     for i in range(2, int(num**0.5) + 1):
@@ -10,27 +11,40 @@ def is_prime(num):
     return True
 
 
-def all_numbers():
-    non_prime = [1]
-    for i in range(3, 1000000):
-        if not is_prime(i):
-            non_prime.append(i)
-    return non_prime
+def prime_count(num):
+    """prime_count"""
+    factors_count = 0
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0 and is_prime(i):
+            factors_count += 1
+            while num % i == 0:
+                num //= i
+    if num > 1:
+        factors_count += 1
+    return factors_count
 
 
-def factors(num):
-    li = []
+def find_integers(consecutive_count, target_factors_count):
+    """find_intergers"""
+    current_count = 0
+    num = 2
+    while True:
+        if prime_count(num) == target_factors_count:
+            current_count += 1
+            if current_count == consecutive_count:
+                return num - consecutive_count + 1
+        else:
+            current_count = 0
+        num += 1
 
-    for i in range(2, int(num**0.5)):
-        if num % i == 0:
-            if is_prime(i):
-                li.append(i)
-            if num // i != i and is_prime(num // i):
-                li.append(i)
 
-    return li
+def answer():
+    """answer()"""
+    consecutive_count = 4
+    target_factors_count = 4
+    result = find_integers(consecutive_count, target_factors_count)
+    return result
 
 
 if __name__ == "__main__":
-    print("Problem no 47")
-    print("Answer of math047 = incomplete = ")
+    print("Answer of math047 = ", answer())
